5f7370aa7d5c1bd2e29ae2751b5743d29c331f60
Marinah1031 <marinahtam@gmail.com>
2023-09-11 11:54:44 -0700

    adding plugins and colorations to the background and theme

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,21 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   initdb();
-console.error('putDb not implemented');
+console.error("putDb not implemented");
 
 const jateDb = await openDB("jate", 1);
 
@@ -29,27 +29,27 @@ store.put(newContent);
 
 await text.done;
 
-console.log('data saved to the database', newContent);
+console.log("data saved to the database", newContent);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   initdb();
-  console.error('getDb not implemented');
+  console.error("getDb not implemented");
 
   const jateDb = await openDB("jate", 1);
 
   const text = jateDb.transaction("jate", "readonly");
 
-  const store = text.objectStore('jate');
+  const store = text.objectStore("jate");
 
   const result = await store.get(1);
-  
+
   if (result !== undefined) {
-    console.log('data retrieved from the database', result.value);
+    console.log("data retrieved from the database", result.value);
     return result.value;
   } else {
-    console.log('data not found in the database');
+    console.log("data not found in the database");
     return null;
   }
 };
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,22 +8,60 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js"
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin ({
+        template: "./index.html",
+        title: "J.A.T.E"
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js"
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "JATE",
+        short_name: "JATE",
+        description: "",
+        background_color: "#28ffe5",
+        theme_color: "#ddb1ff",
+        crossorigin: "use-credentials", // or 'anonymous' if needed
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"), // Replace with your icon path
+            destination: path.join("assets", "icons"),
+            sizes: [16, 32, 96, 144],
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presents: ["@babel/present-env"],
+              plugins: ["@babel/plugin-proposal-object-rest-spread", "@babel/transform-runtime"],
+            },
+          },
+        },
       ],
     },
   };
