5080bc3a9d271f778b69138284d84016737f1a9b
Marinah1031 <marinahtam@gmail.com>
2023-09-11 11:01:26 -0700

    adding logic to database using a method that accepts some and all conent

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,32 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+console.error('putDb not implemented');
+const jateDb = await openJateDB();
+const tx = jateDb.transaction('jate', 'readwrite');
+const store = tx.objectStore('jate');
+await store.put({ value: content });
+await tx.done;
+console.log('data saved to the database', content);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async () => {
+  console.error('getDb not implemented');
+  const jateDb = await openJateDB();
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const result = await store.get(1);
+  if (result !== undefined) {
+    console.log('data retrieved from the database', result.value);
+    return result.value;
+  } else {
+    console.log('data not found in the database');
+    return null;
+  }
+};
 initdb();
+
+
+export { putDb, getDb };
