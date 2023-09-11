b905593a4eab5acb0d8864df27f5e230397f62c0
Marinah1031 <marinahtam@gmail.com>
2023-09-11 12:03:22 -0700

    redoing some code and trying to fix error for css

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,56 +1,39 @@
-import { openDB } from "idb";
+import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB("jate", 1, {
+  openDB('jate', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains("jate")) {
-        console.log("jate database already exists");
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
         return;
       }
-      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
-      console.log("jate database created");
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  initdb();
-console.error("putDb not implemented");
-
-const jateDb = await openDB("jate", 1);
-
-const text = jateDb.transaction("jate", "readwrite");
-
-const store = tx.objectStore("jate");
-
-const newContent = {jate: content };
-
-store.put(newContent);
-
-await text.done;
-
-console.log("data saved to the database", newContent);
+  console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result.value);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  initdb();
-  console.error("getDb not implemented");
-
-  const jateDb = await openDB("jate", 1);
-
-  const text = jateDb.transaction("jate", "readonly");
-
-  const store = text.objectStore("jate");
-
-  const result = await store.get(1);
-
-  if (result !== undefined) {
-    console.log("data retrieved from the database", result.value);
-    return result.value;
-  } else {
-    console.log("data not found in the database");
-    return null;
-  }
-};
+  console.log('GET from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  result
+    ? console.log('🚀 - data retrieved from the database', result.value)
+    : console.log('🚀 - data not found in the database');
+    return result?.value;
+  };
 initdb();
