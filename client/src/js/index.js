9c52d9087aa5942f09d345c70b428b57b310205e
Marinah1031 <marinahtam@gmail.com>
2023-09-11 11:39:58 -0700

    adding caching, fixing bugs and testing

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,14 +1,14 @@
-import { Workbox } from 'workbox-window';
-import Editor from './editor';
-import './database';
-import '../css/style.css';
+import { Workbox } from "workbox-window";
+import Editor from "./editor";
+import "./database";
+import "../css/style.css";
 
-const main = document.querySelector('#main');
+const main = document.querySelector("#main");
 main.innerHTML = '';
 
 const loadSpinner = () => {
-  const spinner = document.createElement('div');
-  spinner.classList.add('spinner');
+  const spinner = document.createElement("div");
+  spinner.classList.add("spinner");
   spinner.innerHTML = `
   <div class="loading-container">
   <div class="loading-spinner" />
@@ -19,7 +19,7 @@ const loadSpinner = () => {
 
 const editor = new Editor();
 
-if (typeof editor === 'undefined') {
+if (typeof editor === "undefined") {
   loadSpinner();
 }
 
