09420d0b9eaf74016440a86cb112a2dbfab3951b
Marinah1031 <marinahtam@gmail.com>
2023-09-11 14:40:39 -0700

    add event handlers and expalnations

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,8 +3,13 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
+//adds an event listener to the 'window' object taht listens for the 'beforeintallprompt' event
+//event is triggered when the PWA is eligible for installation on a user's device and the brower to display intall prompt
 window.addEventListener('beforeinstallprompt', (event) => {
+    //inside the event handler, it assingns the 'event' object to a property called 'deferredPrompt' on the window object
     window.deferredPrompt = event;
+    //this toggles the 'hidden' class on an element with the variable name 'butInstall
+    //the classList.toggle method is used to add the'hiddnen' class to the element when the second arguemnt is 'false' 
     butInstall.classList.toggle('hidden', false);
 });
 
